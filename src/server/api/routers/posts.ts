d340ee8ff9b6c52452d762d5a8f4ec298caefa59
peternguyen777 <peter.quang.nguyen@gmail.com>
2023-08-12 17:52:15 +1000

    add PostView

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,8 +1,41 @@
-import { z } from "zod";
+import { clerkClient } from "@clerk/nextjs";
+import type { User } from "@clerk/nextjs/dist/types/server";
+import { TRPCError } from "@trpc/server";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
+const filterUserForClient = (user: User) => ({
+  id: user.id,
+  username: user.username,
+  profilePicture: user.imageUrl,
+});
+
 export const postsRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.prisma.post.findMany({ take: 100 });
+
+    const users = await clerkClient.users.getUserList({
+      userId: posts.map((post) => post.authorId),
+      limit: 100,
+    });
+
+    const mapUsers = users.map(filterUserForClient);
+
+    return posts.map((post) => {
+      const author = mapUsers.find((user) => user.id === post.authorId);
+
+      if (!author?.username || !author)
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Author for post not found",
+        });
+
+      return {
+        post,
+        author: {
+          ...author,
+          username: author.username,
+        },
+      };
+    });
   }),
 });
