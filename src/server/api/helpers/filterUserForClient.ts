86331f491f233dcb61103e6401519059e3f36d07
peternguyen777 <peter.quang.nguyen@gmail.com>
2023-08-13 19:19:18 +1000

    allow google oauth

--- src/server/api/helpers/filterUserForClient.ts
+++ src/server/api/helpers/filterUserForClient.ts
@@ -2,6 +2,6 @@ import type { User } from "@clerk/nextjs/dist/types/server";
 
 export const filterUserForClient = (user: User) => ({
   id: user.id,
-  username: user.username,
+  username: user.emailAddresses[0]?.emailAddress,
   profilePicture: user.imageUrl,
 });
--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -51,10 +51,7 @@ export const postsRouter = createTRPCRouter({
 
       return {
         post,
-        author: {
-          ...author,
-          username: author.username,
-        },
+        author,
       };
     });
   }),
